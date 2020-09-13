fe6cd5cf9ea620a5144131c43d6094011bb6871e
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2020-09-13 10:11:24 +0200

    Removed manual implementation of a progress bar. Was replaced by tqdm.

--- utilities.py
+++ utilities.py
@@ -29,32 +29,6 @@ log.addHandler(logging.NullHandler())
 #   UTILITIES
 #
 
-# Displays or updates a console progress bar
-def update_progress(progress):
-    """
-    update_progress() : Displays or updates a console progress bar
-    Accepts a float between 0 and 1. Any int will be converted to a float.
-    A value under 0 represents a 'halt'.
-    A value at 1 or bigger represents 100%
-    """
-    barLength = 40 # Modify this to change the length of the progress bar
-    status = ""
-    if isinstance(progress, int):
-        progress = float(progress)
-    if not isinstance(progress, float):
-        progress = 0
-        status = "error: progress var must be float\r\n"
-        log.debug("utilities.update_progress expects int or float as input. Given argument: " + str(progress))
-    if progress < 0:
-        progress = 0
-        status = "Halt...\r\n"
-    if progress >= 1:
-        progress = 1
-        status = "Done...\r\n"
-    block = int(round(barLength*progress))
-    text = "\rRunning: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), round(progress*100, 1), status)
-    sys.stdout.write(text)
-    sys.stdout.flush()
 
 def readFileAsText(path):
     """
