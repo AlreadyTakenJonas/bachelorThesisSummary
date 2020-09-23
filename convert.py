30b43be178ee7abf2740bbb646ce4dc7b5a363d9
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2020-09-23 12:28:12 +0200

    Testing around. Trying to find the bugs.

--- convert.py
+++ convert.py
@@ -75,14 +75,39 @@ def __monteCarlo(param):
                     [np.sin(zeta),  np.cos(zeta), 0],
                     [0           ,  0           , 1]    ])
 
+    Rcos = np.array([   [np.cos(phi)*np.cos(theta)*np.cos(zeta)-np.sin(phi)*np.sin(zeta),      np.sin(phi)*np.cos(theta)*np.cos(zeta)+np.cos(phi)*np.sin(zeta),       -np.sin(phi)*np.cos(zeta)],
+                        [-np.cos(phi)*np.cos(theta)*np.sin(zeta)-np.sin(phi)*np.cos(zeta),     -np.sin(phi)*np.cos(theta)*np.sin(zeta)+np.cos(phi)*np.cos(zeta),       np.sin(phi)*np.sin(zeta)],
+                        [np.cos(phi)*np.sin(theta),                                            np.sin(phi)*np.sin(theta),                                              np.cos(theta)            ]  ])
+
     # Calculate the first half of the rotation
     transposed = Rz.T @ Ry.T @ Rx.T
 
     # Rotate every raman tensor
     for index, tensor in enumerate(tensorlist):
 
+    #    result.append( {"head" : tensor["head"],
+    #                    "matrix" : transposed @ tensor["matrix"] @ Rx @ Ry @ Rz })
+
+        #matrix = np.linalg.inv(Rcos) @ tensor["matrix"] @ Rcos
+
+        matrix = transposed @ tensor["matrix"] @ Rx @ Ry @ Rz
+
+        azz = matrix[2,2]**2
+        axz = matrix[0,2]**2
+
+        #print("EIGENVALUE: " + tensor["head"])
+        #print( np.linalg.eigvals(matrix))
+
+        incomingLight = np.array([0,0,1])
+        scatteredLight = ( matrix @ incomingLight )**2
+        # Divide the intensity of the light orthogonal to the intial light polarsation by the intensity of the light parallel polarised to the inital polarisation
+        #terminalDepolarisationRatio = scatteredLight[0]**2 / scatteredLight[2]**2
+
         result.append( {"head" : tensor["head"],
-                        "matrix" : transposed @ tensor["matrix"] @ Rx @ Ry @ Rz })
+                        "matrix" : matrix,
+                        "scattered": scatteredLight,
+                        "axz_square": axz,
+                        "azz_square": azz })
 
     # Return rotated tensors
     return result
@@ -116,16 +141,18 @@ def main(cliArgs):
     convertedTensorlist = [{"head": tensor["head"],
                             "matrix": np.array([ [0, 0, 0],
                                                  [0, 0, 0],
-                                                 [0, 0, 0]  ]).astype(np.float)
+                                                 [0, 0, 0]  ]).astype(np.float),
+                            "scattered": np.array([0,0,0]),
+                            "axz_square": 0,
+                            "azz_square": 0
                            } for tensor in tensorlist_input]
     # Scale the original tensorlist down by a factor of iterationLimit to make sure that the sum over all iterations will equal the mean over all iterations
     tensorlist = [{ "head": tensor["head"],
-                    "matrix": tensor["matrix"]/cliArgs.iterationLimit } for tensor in tensorlist_input]
+                    "matrix": tensor["matrix"] } for tensor in tensorlist_input]
 
     # Build a generator that returns a tuple that can be passed to the monte-carlo-simulation function
     # It contains one tuple for every iteration. The tuples contain three random anlges in radians and the tensorlist, that will be rotated.
     processArgs = ( (rand.random() * 2*np.pi, rand.random() * 2*np.pi, rand.random() * 2*np.pi, tensorlist) for i in range(cliArgs.iterationLimit) )
-
 
 # RUN MONTE-CARLO SIMULATION
 # Calculation:  1. M(phi, theta, zeta) = (R_z)^T (R_y)^T (R_x)^T a_mol R_x R_y R_z
@@ -136,6 +163,9 @@ def main(cliArgs):
     log.info("START MONTE CARLO SIMULATION")
 
     # !!!!! LOGGING IS OMITTED DURING THE SIMULATION DUE TO SEVERE PERFORMANCE ISSUES !!!!!!
+
+    # Clear test log file
+    open('log/convergence.txt', 'w').close()
 
     # Create a pool of workers sharing the computation task
     with multiprocessing.Pool(processes = cliArgs.processCount) as pool:
@@ -151,7 +181,17 @@ def main(cliArgs):
                                      desc = "Processes " + str(cliArgs.processCount) ):
             # Tally the results of all processes up to get the mean of all computations
             convertedTensorlist = [ {"head": tensor["head"],
-                                     "matrix": np.add(convertedTensorlist[index]["matrix"], tensor["matrix"]) } for (index, tensor) in enumerate(result) ]
+                                     "matrix": np.add(convertedTensorlist[index]["matrix"], tensor["matrix"]/cliArgs.iterationLimit),
+                                     "scattered": np.add(convertedTensorlist[index]["scattered"], tensor["scattered"]),
+                                     "axz_square": convertedTensorlist[index]["axz_square"] + tensor["axz_square"],
+                                     "azz_square": convertedTensorlist[index]["azz_square"] + tensor["azz_square"] } for (index, tensor) in enumerate(result) ]
+
+            # Log depolarisation ratio to file
+            with open("log/convergence.txt", "a") as file:
+                depol = ""
+                for dict in convertedTensorlist:
+                    depol += str( dict["axz_square"] / dict["azz_square"] ) + "   "
+                file.write(depol + "\n")
 
 
     log.info("STOPPED MONTE CARLO SIMULATION SUCCESSFULLY")
@@ -169,6 +209,8 @@ def main(cliArgs):
 
         log.debug("Check matrix '" + input["head"] + "'.")
 
+        print("\n" + input["head"] + "  " + output["head"])
+
         # Check if loop is comparing the right matrices
         if input["head"] != output["head"]:
             log.critical("INTERNAL ERROR: The header of input and output matrices don't match! Error in input tensor '" + input["head"] + "' and output matrix '" + output["head"] + "'." )
@@ -185,23 +227,42 @@ def main(cliArgs):
             log.critical("TERMINATE EXECUTION.")
             sys.exit(-1)
 
+
+        print("Eigenvalue initial: " + str(eigenvalues) )
+        print("Egenvalue terminal: " + str(np.linalg.eigvals(output["matrix"])))
         # Compute depolarisation ration of the inital tensor via the eigenvalues. See "Angluar Momentum" p.129.
         isotropicPolarisability = sum(eigenvalues)/3
         anisotropicPolarisability_squared = ( (eigenvalues[0]-eigenvalues[1])**2 + (eigenvalues[1]-eigenvalues[2])**2 + (eigenvalues[2]-eigenvalues[0])**2 )/2
         initialDepolarisationRatio = 3*anisotropicPolarisability_squared / ( 45*isotropicPolarisability**2 + 4*anisotropicPolarisability_squared )
 
+
+        print("ScatterdLight: " + str(output["scattered"]))
         # Compute depolarisation ratio of simulation result by comparing the polarisation change, when an e-field vector gets scattered
-        incomingLight = np.array([1,0,0])
-        scatteredLight = output["matrix"] @ incomingLight
+        #incomingLight = np.array([0,0,1])
+        #scatteredLight = output["matrix"] @ incomingLight
         # Divide the intensity of the light orthogonal to the intial light polarsation by the intensity of the light parallel polarised to the inital polarisation
-        terminalDepolarisationRatio = scatteredLight[1]**2 / scatteredLight[0]**2
+        #terminalDepolarisationRatio = scatteredLight[0]**2 / scatteredLight[2]**2
+        terminalDepolarisationRatio = output["scattered"][0] / output["scattered"][2]
+        print("Terminal Depolarisation via scattering: " + str(terminalDepolarisationRatio))
+
+        rand.random()*2*np.pi
+        division = output["axz_square"] / output["azz_square"]
+        print("Terminal Depol via a_xz/a_zz: " + str(division))
+
+        eigenvalues = np.linalg.eigvals(output["matrix"])
+        isotropicPolarisability = sum(eigenvalues)/3
+        anisotropicPolarisability_squared = ( (eigenvalues[0]-eigenvalues[1])**2 + (eigenvalues[1]-eigenvalues[2])**2 + (eigenvalues[2]-eigenvalues[0])**2 )/2
+        terminalDepolarisationRatio = 3*anisotropicPolarisability_squared / ( 45*isotropicPolarisability**2 + 4*anisotropicPolarisability_squared )
+        print("Terminal Depolr. via Eigenval:" + str(terminalDepolarisationRatio))
+
+        print("Initial Depolar.:" + str(initialDepolarisationRatio))
 
         # Check results
-        if round(initialDepolarisationRatio, 7) != round(terminalDepolarisationRatio, 7):
-            log.critical("Validation failed for matrix '" + output["head"] + "'!")
-            log.critical("Input: " + str(round(initialDepolarisationRatio, 7)) + "      Simulation: " + str(round(terminalDepolarisationRatio, 7)))
-            log.critical("TERMINATE EXECUTION.")
-            sys.exit(-1)
+        #if round(initialDepolarisationRatio, 7) != round(terminalDepolarisationRatio, 7):
+        #    log.critical("Validation failed for matrix '" + output["head"] + "'!")
+        #    log.critical("Input: " + str(round(initialDepolarisationRatio, 7)) + "      Simulation: " + str(round(terminalDepolarisationRatio, 7)))
+        #    log.critical("TERMINATE EXECUTION.")
+            #sys.exit(-1)
 
     log.info("Validation done.")
 
@@ -226,3 +287,20 @@ def main(cliArgs):
     cliArgs.outputfile.write_text(output_text)
 
     log.info("STOPPED RAMAN TENSOR CONVERSION SUCCESSFULLY")
+
+
+    phi = rand.random()*2*np.pi
+    theta = rand.random()*2*np.pi
+    zeta = rand.random()*2*np.pi
+    Rcos = np.array([   [np.cos(phi)*np.cos(theta)*np.cos(zeta)-np.sin(phi)*np.sin(zeta),      np.sin(phi)*np.cos(theta)*np.cos(zeta)+np.cos(phi)*np.sin(zeta),       -np.sin(theta)*np.cos(zeta)],
+                        [-np.cos(phi)*np.cos(theta)*np.sin(zeta)-np.sin(phi)*np.cos(zeta),     -np.sin(phi)*np.cos(theta)*np.sin(zeta)+np.cos(phi)*np.cos(zeta),       np.sin(phi)*np.sin(zeta)],
+                        [np.cos(phi)*np.sin(theta),                                            np.sin(phi)*np.sin(theta),                                              np.cos(theta)            ]  ])
+    RcMult = Rcos @ Rcos.T
+    print("normal")
+    print(Rcos)
+    print("transpose")
+    print(Rcos.T)
+    print("inverse")
+    print(np.linalg.inv(Rcos))
+    print("prod")
+    print(RcMult)
