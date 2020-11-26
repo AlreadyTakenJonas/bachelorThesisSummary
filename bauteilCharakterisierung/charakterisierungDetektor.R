96f069faeb15bd6754f27edafd25c471f1e726c0
Jonas Eichhorn <eichhorn.jonas@uni-jena.de>
2020-11-26 18:02:24 +0100

    Plotted all spectra as 3d surface; needs to be prettyfied

--- bauteilCharakterisierung/charakterisierungDetektor.R
+++ bauteilCharakterisierung/charakterisierungDetektor.R
@@ -29,19 +29,72 @@ plot.detector.whitelamp(data=makeSpectraPlotable(detector.spectra[[1]],
                         title="The Changing Detector Response For Different Linear Polarised White Light Of The WiTecs Detector And Microscope")
 
 
+# How does the sensitivity of the detector change with the wavenumber
+ggplot( data = data.frame(wavenumber = detector.spectra[[2]]$wavenumber,
+                          difference = apply(detector.spectra[[2]][,-1], 1, function(slice) { max(slice)-min(slice) }) 
+                          ),
+        mapping = aes(x=wavenumber, y=difference)) +
+  theme_hot() +
+  labs(title = "Difference between maximal and minimal detector response",
+       x = expression(bold("wavenumber / cm"^"-1")),
+       y = "count difference") +
+  geom_line()
 
-colnames(detector.spectra[[2]][,-1])[apply(detector.spectra[[2]][,-1],1,which.max)]
+# How does the sensitivity of the detector change with the wavenumber (difference method)
+ggplot( data = data.frame(wavenumber = detector.spectra[[2]]$wavenumber,
+                          difference = apply(detector.spectra[[2]][,-1], 1, function(slice) { max(slice)-min(slice) }) 
+                        ),
+        mapping = aes(x=wavenumber, y=difference) ) +
+  theme_hot() +
+  labs(title = "Difference between maximal and minimal detector response",
+       x = expression(bold("wavenumber / cm"^"-1")),
+       y = "count difference") +
+  geom_line()
 
- #plot(detector.spectra[[2]][,1], as.numeric(colnames(detector.spectra[[2]])[-1]), col=detector.spectra[[2]][,-1])
 
-#plot(apply(detector.spectra[[2]][,-1],1,function(x) x/max(x)),type="l")
-#colnames(detector.spectra[[2]])[apply(detector.spectra[[2]][,-1],1,which.min)]
-#apply(detector.spectra[[2]][,-1],1,which.min)
+# How does the sensitivity of the detector change with the wavenumber (quotient method)
+ggplot( data = data.frame(wavenumber = detector.spectra[[2]]$wavenumber,
+                          quotient   = apply(detector.spectra[[2]][,-1], 1, function(slice) { min(slice)/max(slice) }) 
+                          ),
+        mapping = aes(x=wavenumber, y=quotient) ) +
+  theme_hot() +
+  labs(title = "Quotient of maximal and minimal detector response",
+       x = expression(bold("wavenumber / cm"^"-1")),
+       y = "min/max") +
+  geom_line()
 
-#plot(detector.spectra[[2]][,1],detector.spectra[[2]][,21],type="l")
-#lines(detector.spectra[[2]][,1],detector.spectra[[2]][,2],type="l",col="red")
-#lines(detector.spectra[[2]][,1],detector.spectra[[2]][,19],type="l",col="red")
 
-#filled.contour(x=detector.spectra[[2]][,1],
-#        y=as.numeric(colnames(detector.spectra[[2]])[-1]),
-#        z=as.matrix(detector.spectra[[2]][,-1]-apply(detector.spectra[[2]][,-1],1,max)))
+
+#
+# Plot all spectra as 3d-surface
+#
+colnames(PlotMat)
+PlotMat <- as.matrix(detector.spectra[[2]][,-c(1,21:24)])
+grid <- list(ordinate=detector.spectra[[2]]$wavenumber,
+             abcissa=as.numeric(colnames(PlotMat)))
+grid.mat <- make.surface.grid(grid)             
+output <- as.surface(grid.mat,PlotMat)
+
+ncol <- 100
+colPal <- colorRampPalette(c("blue","red"))(ncol)
+zfacet <- PlotMat[-1, -1] + PlotMat[-1, -ncol(PlotMat)] + PlotMat[-nrow(PlotMat), -1] + PlotMat[-nrow(PlotMat), -ncol(PlotMat)]
+facetcol <- cut(zfacet, ncol)
+plotCol <- persp(output,border=NA,col=colPal[facetcol],phi=20,theta=270)
+
+fields::plot.surface(output,type="p",theta=270,border=NA)
+
+# Add gridlines
+resX <- 20
+resY <- 2
+selX <- seq(1,length(grid[[1]]),by=resX)
+selY <- seq(1,length(grid[[2]]),by=resY)
+xGrid <- grid[[1]][selX]
+yGrid <- grid[[2]][selY]
+
+for(i in selX) lines(trans3d(x=rep(grid[[1]][i],ncol(PlotMat)),
+                             y=grid[[2]],
+                             z=PlotMat[i,],pmat=plotCol))
+
+for(i in selY) lines(trans3d(x=grid[[1]],
+                             y=rep(grid[[2]][i],nrow(PlotMat)),
+                             z=PlotMat[,i],pmat=plotCol))
