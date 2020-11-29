7c1d38824029f3a73aaf37f30a301b692ca00a16
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2020-11-29 14:02:24 +0100

    Continue report: waveplate done, optical fibers started

--- bauteilCharakterisierung/charakterisierungW1.R
+++ bauteilCharakterisierung/charakterisierungW1.R
@@ -2,7 +2,7 @@ require(RHotStuff)
 require(ggplot2)
 
 # Extract data from eLabFTW
-data <- GET.elabftw.byselector(27, header = T)[[1]]
+W1.data <- GET.elabftw.byselector(27, header = T)[[1]]
 
 # Replace NA values by using the previous value in the vector
 fillVector <- function(vector) {
@@ -11,21 +11,21 @@ fillVector <- function(vector) {
   }
   return(vector)
 }
-data$Y2 <- fillVector(data$Y2)
-data$Y4 <- fillVector(data$Y4)
+W1.data$Y2 <- fillVector(W1.data$Y2)
+W1.data$Y4 <- fillVector(W1.data$Y4)
 
 # Normalise
-data$Y1 <- data$Y1/data$Y2 *100
-data$Y3 <- data$Y3/data$Y4 *100
+W1.data$Y1 <- (W1.data$Y1/W1.data$Y2) %>% `/`(., max(.)) *100
+W1.data$Y3 <- W1.data$Y3/W1.data$Y4 *100
 
 # Plot
-ggplot(data = data, mapping = aes(x = X, y=Y3)) +
+ggplot(data = W1.data, mapping = aes(x = X, y=Y3)) +
   geom_point() +
   theme_minimal() +
   labs(title = "Durchlässigkeit der Wellenplatte W1 in Abhängigkeit des Rotationswinkels",
        x = "Rotationswinkel / °",
        y = "Anteil der Strahlung, die die Wellenplatte passiert / %")
-ggplot(data = data, mapping = aes(x=X, y=Y1)) +
+ggplot(data = W1.data, mapping = aes(x=X, y=Y1)) +
   geom_point(size=0.5) +
   theme_minimal() +
   labs(title = "Nullpunktsbestimmung der Wellenplatte W1",
--- bauteilCharakterisierung/charakterisierungW2.R
+++ bauteilCharakterisierung/charakterisierungW2.R
@@ -2,7 +2,7 @@ require(RHotStuff)
 require(ggplot2)
 
 # Extract data from eLabFTW
-data <- GET.elabftw.byselector(28, header = T)[[1]]
+W2.data <- GET.elabftw.byselector(28, header = T)[[1]]
 
 # Replace NA values by using the previous value in the vector
 fillVector <- function(vector) {
@@ -11,23 +11,23 @@ fillVector <- function(vector) {
   }
   return(vector)
 }
-data$Y2 <- fillVector(data$Y2)
-data$Y4 <- fillVector(data$Y4)
+W2.data$Y2 <- fillVector(W2.data$Y2)
+W2.data$Y4 <- fillVector(W2.data$Y4)
 
 # Normalise
-data$Y1 <- data$Y1/data$Y2 *100
-data$Y3 <- data$Y3/data$Y4 *100
+W2.data$Y1 <- (W2.data$Y1/W2.data$Y2) %>% `/`(., max(.)) *100
+W2.data$Y3 <- W2.data$Y3/W2.data$Y4 *100
 
 # Plot
-ggplot(data = data, mapping = aes(x = X, y=Y3)) +
+ggplot(data = W2.data, mapping = aes(x = X, y=Y3)) +
   geom_point() +
   theme_minimal() +
-  labs(title = "Durchlässigkeit der Wellenplatte W1 in Abhängigkeit des Rotationswinkels",
+  labs(title = "Durchlässigkeit der Wellenplatte W2 in Abhängigkeit des Rotationswinkels",
        x = "Rotationswinkel / °",
        y = "Anteil der Strahlung, die die Wellenplatte passiert / %")
-ggplot(data = data, mapping = aes(x=X, y=Y1)) +
+ggplot(data = W2.data, mapping = aes(x=X, y=Y1)) +
   geom_point(size=0.5) +
   theme_minimal() +
-  labs(title = "Nullpunktsbestimmung der Wellenplatte W1",
+  labs(title = "Nullpunktsbestimmung der Wellenplatte W2",
        x = "Rotationswinkel / °",
        y = "Anteil der Strahlung, die den Analysator passiert / %")
