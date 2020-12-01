09e8549caf23a5248c552ed10aba55cb61fdd7e6
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2020-12-01 17:24:31 +0100

    Compared polarisation ratio of prediced and measured stokes vectors

--- bauteilCharakterisierung/charakterisierungFaser_F3.R
+++ bauteilCharakterisierung/charakterisierungFaser_F3.R
@@ -92,16 +92,29 @@ F3.muellermatrix <- matrix( c( limSolve::Solve(as.matrix(F3.mueller.stokes$PRE[,
 F3.predicted.POST <- apply(F3.mueller.stokes$PRE[,c(2,3,4)], 1, function(stokes) { 
   F3.muellermatrix %*% ( stokes %>% unlist %>% c(., 0) ) 
 })
+# Calculate polarisation ratio of predicted stokes vectors
+F3.predicted.POST.polarisation <- data.frame( "predicted" = apply(F3.predicted.POST, 2, function(stokes) { 
+                                                                  sqrt(sum(stokes[c(2,3,4)]^2)) / stokes[1] } ),
+                                              "measured"  = apply(F3.mueller.stokes$POST, 1, function(stokes) { 
+                                                                  sqrt(sum(stokes[c(3,4)]^2))   / stokes[2] } ) )
 # Plot and compare the predicted and measured stokes parameters
 # S0
-plot(x = F3.mueller.stokes$POST$W, y = F3.mueller.stokes$POST$S0, col="red", type="l")
+plot(x = F3.mueller.stokes$POST$W, y = F3.mueller.stokes$POST$S0, col="red", type="l",
+     main=expression("Predicted/Measured S"[0]))
 lines(x = F3.mueller.stokes$POST$W, y = F3.predicted.POST[1,], col="blue")
 # S1
-plot(x = F3.mueller.stokes$POST$W, y = F3.mueller.stokes$POST$S1, col="red", type="l")
+plot(x = F3.mueller.stokes$POST$W, y = F3.mueller.stokes$POST$S1, col="red", type="l",
+     main=expression("Predicted/Measured S"[1]))
 lines(x = F3.mueller.stokes$POST$W, y = F3.predicted.POST[2,], col="blue")
 # S2
-plot(x = F3.mueller.stokes$POST$W, y = F3.mueller.stokes$POST$S2, col="red", type="l")
+plot(x = F3.mueller.stokes$POST$W, y = F3.mueller.stokes$POST$S2, col="red", type="l",
+     main=expression("Predicted/Measured S"[2]))
 lines(x = F3.mueller.stokes$POST$W, y = F3.predicted.POST[3,], col="blue")
+# Polarisation ratio
+plot(x = F3.mueller.stokes$POST$W, y = F3.predicted.POST.polarisation$predicted, col="blue", type="l",
+     main=expression("Predicted/Measured "*Pi))
+lines(x = F3.mueller.stokes$POST$W, y = F3.predicted.POST.polarisation$measured, col="red")
+
 
 # Calculate difference between predicted and measured stokes parameters
 # Mean difference and standard deviation between measurement and prediction
@@ -110,3 +123,11 @@ lines(x = F3.mueller.stokes$POST$W, y = F3.predicted.POST[3,], col="blue")
 # Relative mean difference and standard deviation between measurement and prediction
 ( t(F3.predicted.POST[-4,]) - F3.mueller.stokes$POST[,c(2,3,4)] ) %>% `/`(., F3.mueller.stokes$POST[,c(2,3,4)]) %>% abs %>% colMeans
 ( t(F3.predicted.POST[-4,]) - F3.mueller.stokes$POST[,c(2,3,4)] ) %>% `/`(., F3.mueller.stokes$POST[,c(2,3,4)]) %>% abs %>% apply(., 2, sd)
+
+# Calculate difference between predicted and measured polarisation ratio
+# Mean difference and standard deviation between measurement and prediction
+(F3.predicted.POST.polarisation$predicted - F3.predicted.POST.polarisation$measured) %>% abs %>% mean
+(F3.predicted.POST.polarisation$predicted - F3.predicted.POST.polarisation$measured) %>% abs %>% sd
+# Relative mean difference and standard deviation between measurement and prediction
+(F3.predicted.POST.polarisation$predicted - F3.predicted.POST.polarisation$measured) %>% `/`(., F3.predicted.POST.polarisation$measured) %>% abs %>% mean
+(F3.predicted.POST.polarisation$predicted - F3.predicted.POST.polarisation$measured) %>% `/`(., F3.predicted.POST.polarisation$measured) %>% abs %>% sd
