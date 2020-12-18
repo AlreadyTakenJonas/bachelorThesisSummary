cd35f55e8e427f5ec1c2e2c7d1daf80ead173dcf
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2020-12-18 08:50:04 +0100

    Preprocess raman spectra of trilaurin and 4-AAP

--- auswertungSpektren/AAP_witec.R
+++ auswertungSpektren/AAP_witec.R
@@ -1,5 +1,5 @@
 #
-# ANALYSING POLARISED RAMAN SPECTRA OF Trilaurin
+# ANALYSING POLARISED RAMAN SPECTRA OF 4-AAP
 #
 
 
@@ -17,24 +17,24 @@ library("baseline")
 
 
 # FETCH DATA
-# Trilaurin Spectra
-trilaurin.spectra.unprocessed <- GET.elabftw.bycaption(84, header=T, outputHTTP=T) %>%
+# AAP Spectra
+AAP.spectra.unprocessed <- GET.elabftw.bycaption(85, header=T, outputHTTP=T) %>%
   parseTimeSeries.elab(., col.spectra=3, sep="") %>% .[[1]]
 
 # PREPROCESS
-trilaurin.spectra <- trilaurin.spectra.unprocessed
+AAP.spectra <- AAP.spectra.unprocessed
 
 #
 # Remove the laser peak from the spectrum
 #
-trilaurin.spectra <- trilaurin.spectra[trilaurin.spectra$wavenumber>230,]
+AAP.spectra <- AAP.spectra[AAP.spectra$wavenumber>150,]
 
 
 # Statistical Background Correction
 #
 # Peaks no longer available
-# trilaurin.spectra[,-1] <- sapply(trilaurin.spectra[,-1], function(spec) spec-Peaks::SpectrumBackground(spec))
-trilaurin.spectra[,-1] <- t( as.matrix(trilaurin.spectra[,-1]) ) %>%
+# AAP.spectra[,-1] <- sapply(AAP.spectra[,-1], function(spec) spec-Peaks::SpectrumBackground(spec))
+AAP.spectra[,-1] <- t( as.matrix(AAP.spectra[,-1]) ) %>%
   baseline::baseline(., method="fillPeaks", lambda=1, it=10, hwi=50, int=2000) %>%
   baseline::getCorrected(.) %>% t(.)
 
@@ -43,22 +43,20 @@ trilaurin.spectra[,-1] <- t( as.matrix(trilaurin.spectra[,-1]) ) %>%
 # Normalise each spectrum
 #
 # Normalisation with hghest point
-# trilaurin.spectra[,-1] <- sapply(trilaurin.spectra[,-1], function(spec) spec/max(spec))
+# AAP.spectra[,-1] <- sapply(AAP.spectra[,-1], function(spec) spec/max(spec))
 # Vector normalisation
-trilaurin.spectra[,-1] <- apply(trilaurin.spectra[,-1], 2, function(spec) spec / sqrt(sum(spec^2)))
+AAP.spectra[,-1] <- apply(AAP.spectra[,-1], 2, function(spec) spec / sqrt(sum(spec^2)))
 
 
 
 # FIND THE LOCATION OF THE PEAKS
 # Plot single spectrum
-plot(x = trilaurin.spectra$wavenumber, y = trilaurin.spectra$`0`, type="l")
+plot(x = AAP.spectra$wavenumber, y = AAP.spectra$`0`, type="l")
 locator()
 
 
 
-
-
 # Plot ALL SPECTRA as 3d SURFACE
-plot.detector.allSpectra.interactable(trilaurin.spectra, 
-                                      title=expression(bold("Normalised Raman Spectra Of Trilaurin For Different Polarised Light")))
+plot.detector.allSpectra.interactable(AAP.spectra, 
+                                      title=expression(bold("Normalised Raman Spectra Of 4-AAP For Different Polarised Light")))
 
